d3bea8cc071c0efd3d1270182ddfd5a5a18c676d
klesio <klesio.dev@gmail.com>
2023-03-03 02:03:24 -0300

    create component loading default

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
+import { useCallback, useState } from 'react';
 import { StatusBar } from 'react-native';
+import { Loading } from './src/components/Loading';
 import { Home } from './src/screens/Home';
 
 export default function App() {
+  const [notLoaded, setLoading] = useState(false);
+
+  if (notLoaded) return <Loading />;
+
   return (
     <>
       <Home />

41ce12e284da153b47b4f3da33758cab137cd0f7
klesio <klesio.dev@gmail.com>
2023-03-03 02:08:13 -0300

    docs(project) : added exemples fonts in project

--- App.tsx
+++ App.tsx
@@ -1,12 +1,31 @@
-import { useCallback, useState } from 'react';
+
 import { StatusBar } from 'react-native';
-import { Loading } from './src/components/Loading';
 import { Home } from './src/screens/Home';
 
-export default function App() {
-  const [notLoaded, setLoading] = useState(false);
+/* import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from '@expo-google-fonts/inter';
 
-  if (notLoaded) return <Loading />;
+import { Loading } from './src/components/Loading'; */
+
+
+export default function App() {
+
+  /*   const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
+
+  if (!fontsLoaded) return <Loading />;
+ */
+
+  /* https://docs.expo.dev/guides/using-custom-fonts/ */
 
   return (
     <>

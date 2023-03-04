2c2f5e6d17560be04f6892b9adfdb0b51dfbf61d
klesio <klesio.dev@gmail.com>
2023-03-03 22:19:28 -0300

    started configs default in apps react native using tailwindcss

---
+++ src/screens/Home.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export function Home() {
+  return (
+    <View className='flex items-center justify-center h-full'>
+      <Text className='text-blue-800'>Olá mundo</Text>
+    </View>
+  );
+}

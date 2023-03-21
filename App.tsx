d4e547a281014277e538579cccb63287ad423af6
klesio <klesio.dev@gmail.com>
2023-03-20 23:16:46 -0300

    feat(project): colocado fontes no projeto e fix bugs

--- App.tsx
+++ App.tsx
@@ -1,26 +1,16 @@
 import { StatusBar } from 'react-native';
 import { Routes } from './src/routes';
 
-/* import {
-  useFonts,
-  Inter_400Regular,
-  Inter_600SemiBold,
-  Inter_700Bold,
-  Inter_800ExtraBold,
-} from '@expo-google-fonts/inter';
+import { useFonts, Archivo_600SemiBold } from '@expo-google-fonts/archivo';
 
-import { Loading } from './src/components/Loading'; */
+import { Loading } from './src/components/Loading';
 
 export default function App() {
-  /*   const [fontsLoaded] = useFonts({
-    Inter_400Regular,
-    Inter_600SemiBold,
-    Inter_700Bold,
-    Inter_800ExtraBold,
+  const [fontsLoaded] = useFonts({
+    Archivo_600SemiBold,
   });
 
   if (!fontsLoaded) return <Loading />;
- */
 
   /* https://docs.expo.dev/guides/using-custom-fonts/ */
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,9 @@ module.exports = {
         background: '#09090a',
       },
       fontFamily: {
-      /*   regular: 'Inter_400Regular',
-        semibold: 'Inter_600SemiBold',
-        bold: 'Inter_700Bold',
-        extrabold: 'Inter_800ExtraBold', */
+        Archivo: ['Archivo_600SemiBold'],
+        PoppinsRegular: ['Poppins_400Regular'],
+        PoppinsSemiBold: ['Poppins_600SemiBold'],
       },
     },
   },

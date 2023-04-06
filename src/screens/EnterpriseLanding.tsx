63638ea28b10f2c3fe1f374cecb5b76bc19fe930
pablo correia <pablocorreia2012@gmail.com>
2023-04-06 00:54:06 -0300

    feat(EnterpriseLanding): ajustes

--- src/screens/EnterpriseLanding.tsx
+++ src/screens/EnterpriseLanding.tsx
@@ -1,6 +1,7 @@
 import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import React from 'react'
+import BackgroundEnterprise from '../assets/svg/Group 11.svg';
 import BackgroundBlue from '../assets/svg/background-blue.svg';
 
 export function EnterpriseLanding() {
@@ -8,17 +9,14 @@ export function EnterpriseLanding() {
 
   return (
    <View className='flex items-center justify-center h-full flex-1 bg-sky-500 px-8 pt-14 '>
-      {/* <Text className='text-blue-200 m-3 font-PoppinsSemiBold'>
-         Pagina do estudante
-      </Text> */}
-      <BackgroundBlue width='310' />
+      <BackgroundBlue width='310'/>
 
       <Text className='text-white my-4 w-52 text-xl leading-8'>
          <Text className='font-ArchivoBold'>Quer ser um parceiro SkillShare?</Text>
       </Text>
 
       <TouchableOpacity activeOpacity={0.7}
-        className='flex-row h-11 bg-blue-500 px-11 border border-blue-600 rounded-lg items-center'
+        className='flex-row h-11 bg-red-500 px-11 border border-red-600 rounded-lg items-center'
         onPress={() => navigate('home')} >
         <Text className='text-white ml-3 font-PoppinsRegular text-base'>
          Ser parceiro

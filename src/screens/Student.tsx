591f2aafb589b958113dab3da3f6200986baf294
pablo correia <pablocorreia2012@gmail.com>
2023-05-04 01:27:01 -0300

    feat(skillers/favorites): criado as paginas de favaoritos e de skillers disponiveis com as suas devidas estilizações

--- src/screens/Student.tsx
+++ src/screens/Student.tsx
@@ -1,23 +1,15 @@
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import React, { useState } from 'react';
+import Menu from '../components/Menu';
 
 export function Student() {
   const { navigate } = useNavigation();
+  const [filterText, setFilterText] = useState('');
 
-  return (
-    <View className='flex items-center justify-center h-full flex-1 bg-slate-700 px-8 pt-14 '>
-    <Text className='text-blue-200 m-3 font-PoppinsSemiBold'>Pagina do estudante</Text>
-
-    <TouchableOpacity
-      activeOpacity={0.7}
-      className='flex-row h-11 px-4 border border-blue-600 rounded-lg items-center'
-      onPress={() => navigate('onboardingPageOne')}
-    >
-      <Text className='text-white ml-3 font-PoppinsRegular text-base'>
-       Voltar para inicio
-      </Text>
-    </TouchableOpacity>
-  </View>
+  return ( 
+    <View className='flex-col justify-start h-full w-full bg-sky-500 pt-14 '>
+      <Menu/>
+    </View>
   );
 }

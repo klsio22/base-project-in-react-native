271525e100dbdc6b9059f2cba82ed8cb6b013f98
Landwixxx <landwixxx@gmail.com>
2023-05-17 21:53:31 -0300

    correção: navegando para a pagina do professor

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -7,11 +7,8 @@ import { OnboardingPageTwo } from '../screens/OnboardingPageTwo';
 import { Home } from '../screens/Home';
 import { Login } from '../screens/Login';
 import { Student } from '../screens/Student';
-<<<<<<< HEAD
 import { ProfessorLanding } from '../screens/ProfessorLanding';
-=======
 import { EnterpriseLanding } from "../screens/EnterpriseLanding";
->>>>>>> 591f2aafb589b958113dab3da3f6200986baf294
 
 export function AppRoutes() {
   return (

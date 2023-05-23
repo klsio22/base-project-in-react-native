007a196ea153e6e02e10c7d404c1caf9a9fc5eea
klesio <klesio.dev@gmail.com>
2023-05-23 17:10:52 -0300

    fix(student): remove comments

--- src/screens/Student.tsx
+++ src/screens/Student.tsx
@@ -1,15 +1,11 @@
-import { Text, TextInput, TouchableOpacity, View } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import React, { useState } from 'react';
+import { View } from 'react-native';
+import React from 'react';
 import Menu from '../components/Menu';
 
 export function Student() {
-  const { navigate } = useNavigation();
-  const [filterText, setFilterText] = useState('');
-
-  return ( 
+  return (
     <View className='flex-col justify-start h-full w-full bg-sky-500 pt-14 '>
-      <Menu/>
+      <Menu />
     </View>
   );
 }

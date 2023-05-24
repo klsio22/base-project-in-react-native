c4a4d0625e03677588df9b3fc4778901fdcc1e53
Landwixxx <landwixxx@gmail.com>
2023-05-24 19:44:47 -0300

    correção: tela cadastro professor

--- src/screens/Skiller.tsx
+++ src/screens/Skiller.tsx
@@ -39,7 +39,7 @@ export function Skiller() {
         <View className='flex flex-row justify-evenly mt-4 w-full font-PoppinsSemiBold text-xl '>
           <TouchableOpacity
             activeOpacity={0.7}
-            className='flex w-28 justify-between items-start p-4 rounded-lg bg-sky-400 '
+            className='flex w-28 h-30 w-30 justify-between items-start p-4 rounded-lg bg-sky-400 '
             onPress={() => navigate('skillers')}>
               
             <ToStudy/>
@@ -51,7 +51,7 @@ export function Skiller() {
 
           <TouchableOpacity
             activeOpacity={0.7}
-            className='flex w-28 justify-between items-start p-4 rounded-lg bg-[#F27C7C] '
+            className='flex w-28 h-30 w-30 justify-between items-start p-4 rounded-lg bg-[#F27C7C] '
             onPress={() => navigate('home')}
           >
             <ToWatch />
@@ -61,16 +61,16 @@ export function Skiller() {
             </Text>
           </TouchableOpacity>
         </View>
-
+        <Divider className='w-full my-4'/>
         <View className='flex items-center w-full justify-start h-full flex-1'>
           <Divider/>   
-          <Text className='font-ArchivoBold text-3xl mt-5 text-stone-600'>
+          <Text className='font-ArchivoBold text-3xl mt-5 text-left w-full text-stone-600'>
             Seus dados
           </Text>
           <TextInput
-            className='w-full mt-4'
+            className='w-full mt-4 bg-slate-100'
             mode='outlined'
-            label="Email"
+            label="Nome Completo"
             value={fullName}
             onChangeText={text => setFullName(text)}
           />

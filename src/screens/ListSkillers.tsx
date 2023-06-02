10e28f89e160736654d64105428c16b91e4339cb
klesio <klesio.dev@gmail.com>
2023-06-01 22:06:49 -0300

    fix(listSkillers): errors in key prop  and SafeAreaView

--- src/screens/ListSkillers.tsx
+++ src/screens/ListSkillers.tsx
@@ -2,9 +2,9 @@ import {
   TextInput,
   TouchableOpacity,
   View,
-  ScrollView,
   FlatList,
   LogBox,
+  SafeAreaView,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import NextScreen from '../assets/svg/next-screen.svg';
@@ -44,13 +44,13 @@ export function ListSkillers() {
     console.log(userDataGet);
   }
 
-  function handleAddFavorite(favId: string) {
+  function handleAddFavorite(favoriteID: string) {
     if (!userData) {
       return; // Retorna se userData for nulo
     }
 
     const array = userData.favorite || [];
-    array.push(favId);
+    array.push(favoriteID);
 
     const userToUpdate = {
       id: user?.uid!!,
@@ -122,75 +122,74 @@ export function ListSkillers() {
         <Divider />
       </View>
 
-      <ScrollView className='w-full h-2/3 px-4 pb-40 bg-[#f0f0f7]'>
-        {filteredData.length > 0 ? (
-          <FlatList
-            data={filteredData}
-            renderItem={({ item }) => (
-              <Card
-                className='p-0 w-90 pb-3 bg-white mb-5 mt-2'
-                key={user?.uid!!}
-              >
-                <View className='p-5'>
-                  <Text className='text-[#32264d] font-PoppinsRegular font-bold text-2xl'>
-                    {item.name}
-                  </Text>
-                  <Text variant='titleSmall'>{item.skills}</Text>
-                </View>
-                {/* <Card.Title title="Roberval dos Santos" subtitle="Acadêmico, 3º período - UTFPR"/> */}
-                <Card.Content className='mb-2 mt-5'>
-                  <Text className='mb-5' variant='bodyMedium'>
-                    {item.bio}
-                  </Text>
-
-                  <Text className='mb-5' variant='bodyMedium'>
-                    {`Por aqui : ${item.link}`}
-                  </Text>
-                  <Divider className='m-2 w-full' />
-                  <View className='flex-row justify-center mt-5'>
-                    <Text className='mr-3'>Preço/hora</Text>
-                    <Text className='text-sky-500 text-bold'>
-                      {`R$ ${item.price}`}
+      <SafeAreaView>
+        <View className='w-full h-full px-4 pb-40 bg-[#f0f0f7]'>
+          {filteredData.length > 0 ? (
+            <FlatList
+              data={filteredData}
+              renderItem={({ item, index }) => (
+                <Card className='p-0 w-90 pb-3 bg-white mb-5 mt-2' key={index}>
+                  <View className='p-5'>
+                    <Text className='text-[#32264d] font-PoppinsRegular font-bold text-2xl'>
+                      {item.name}
                     </Text>
+                    <Text variant='titleSmall'>{item.skills}</Text>
                   </View>
-                </Card.Content>
-                <Card.Actions className='flex justify-center bg-[#fafafc]'>
-                  <TouchableOpacity
-                    activeOpacity={0.7}
-                    className='flex w-18 bg-sky-300 rounded-md justify-center p-3.5'
-                    onPress={() => handleAddFavorite(user?.uid!!)}
-                  >
-                    <Coracao />
-                  </TouchableOpacity>
-                  <TouchableOpacity
-                    activeOpacity={0.7}
-                    className='flex-1 flex-row flex bg-green-900 rounded-md justify-center'
-                    onPress={() => navigate('home')}
-                  >
-                    <Text className='text-white ml-3 p-3 text-base font-PoppinsRegular'>
-                      {' '}
-                      <ZapZap /> Entrar em contato
+                  {/* <Card.Title title="Roberval dos Santos" subtitle="Acadêmico, 3º período - UTFPR"/> */}
+                  <Card.Content className='mb-2 mt-5'>
+                    <Text className='mb-5' variant='bodyMedium'>
+                      {item.bio}
                     </Text>
-                  </TouchableOpacity>
-                </Card.Actions>
-              </Card>
-            )}
-            keyExtractor={(item) => item.id}
-          />
-        ) : (
-          <View className='flex items-center justify-center h-full w-full bg-white'>
-            <Text className='text-[#57534E] my-10 p-4 w-full h-auto'>
-              <Text className='font-ArchivoBold  text-2xl'>
-                Os melhores skillers
+
+                    <Text className='mb-5' variant='bodyMedium'>
+                      {`Por aqui : ${item.link}`}
+                    </Text>
+                    <Divider className='m-2 w-full' />
+                    <View className='flex-row justify-center mt-5'>
+                      <Text className='mr-3'>Preço/hora</Text>
+                      <Text className='text-sky-500 text-bold'>
+                        {`R$ ${item.price}`}
+                      </Text>
+                    </View>
+                  </Card.Content>
+                  <Card.Actions className='flex justify-center bg-[#fafafc]'>
+                    <TouchableOpacity
+                      activeOpacity={0.7}
+                      className='flex w-18 bg-sky-300 rounded-md justify-center p-3.5'
+                      onPress={() => handleAddFavorite(item.id)}
+                    >
+                      <Coracao />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                      activeOpacity={0.7}
+                      className='flex-1 flex-row flex bg-green-900 rounded-md justify-center'
+                      onPress={() => navigate('home')}
+                    >
+                      <Text className='text-white ml-3 p-3 text-base font-PoppinsRegular'>
+                        {' '}
+                        <ZapZap /> Entrar em contato
+                      </Text>
+                    </TouchableOpacity>
+                  </Card.Actions>
+                </Card>
+              )}
+              keyExtractor={(item, index) => index.toString()}
+            />
+          ) : (
+            <View className='flex items-center justify-center h-full w-full bg-white'>
+              <Text className='text-[#57534E] my-10 p-4 w-full h-auto'>
+                <Text className='font-ArchivoBold  text-2xl'>
+                  Os melhores skillers
+                </Text>
+                {'\n'}
+                <Text className='font-ArchivoBold text-2xl '>aparecerão</Text>
+                {'\n'}
+                <Text className='font-ArchivoBold text-2xl '>aqui!!</Text>
               </Text>
-              {'\n'}
-              <Text className='font-ArchivoBold text-2xl '>aparecerão</Text>
-              {'\n'}
-              <Text className='font-ArchivoBold text-2xl '>aqui!!</Text>
-            </Text>
-          </View>
-        )}
-      </ScrollView>
+            </View>
+          )}
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
